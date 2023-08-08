0f031f314ad238af59960c4c7c3a6f9521cccd41
Benjamin <108634496+Oblakoru@users.noreply.github.com>
2023-08-08 18:41:25 +0200

    Dodan dispose ter barve
    Vsaki instanci widgeta sem dodal initState, ki ob gradnji widgeta poskrbi, da je glasnost playerja 0. Prav tako pa sem dodal barve na ikone, ki se spreminjajo glede na to ali se player predvaja ali ne.

--- lib/audio_player.dart
+++ lib/audio_player.dart
@@ -8,7 +8,10 @@ class RelaxingPlayer extends StatefulWidget {
   final String potDoZvoka;
   final IconData iconData;
 
-  const RelaxingPlayer({required this.imeZvoka, required this.potDoZvoka, required this.iconData});
+  const RelaxingPlayer(
+      {required this.imeZvoka,
+      required this.potDoZvoka,
+      required this.iconData});
 
   @override
   _RelaxingPlayerState createState() => _RelaxingPlayerState();
@@ -20,25 +23,51 @@ class _RelaxingPlayerState extends State<RelaxingPlayer> {
   var player = AudioPlayer();
 
   @override
+  void initState() {
+    super.initState();
+
+    // Set your variables here
+    player.setVolume(0);
+  }
+
+  @override
+  void dispose() {
+    // Cleanup resources in the dispose method
+    player.dispose(); // Cancel the stream subscription
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    //player.setVolume(0);
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Icon(widget.iconData, size: 50, color: isPlaying ? Color.fromRGBO(217, 202, 179, 1.0) : Color.fromRGBO(246, 246, 246, 1)),
+        Icon(
+          widget.iconData,
+          size: 50,
+          color: isPlaying
+              ? const Color.fromRGBO(217, 202, 179, 1.0)
+              : const Color.fromRGBO(246, 246, 246, 1),
+        ),
         Container(
           alignment: Alignment.center,
-          child: Text(widget.imeZvoka, style: TextStyle(color: Color.fromRGBO(246, 246, 246, 1), fontSize: 20),),
           width: 100,
+          child: Text(
+            widget.imeZvoka,
+            style: const TextStyle(
+                color: Color.fromRGBO(246, 246, 246, 1), fontSize: 20),
+          ),
         ),
-        SizedBox(
+        const SizedBox(
           width: 20,
         ),
         FloatingActionButton(
-          backgroundColor: Color.fromRGBO(217, 202, 179, 1.0),
-          foregroundColor: Color.fromRGBO(246, 246, 246, 1),
-          hoverColor: Color.fromRGBO(109, 152, 134, 1),
-          
+            backgroundColor: const Color.fromRGBO(217, 202, 179, 1.0),
+            foregroundColor: const Color.fromRGBO(246, 246, 246, 1),
+            hoverColor: const Color.fromRGBO(109, 152, 134, 1),
             onPressed: () async {
               if (isPlaying) {
                 await player.pause();
@@ -59,8 +88,11 @@ class _RelaxingPlayerState extends State<RelaxingPlayer> {
         //      await player1.stop(); // will immediately start playing
         //    },
         //    child:  Icon(isPlaying ? Icons.play_arrow : Icons.stop)),
+        const SizedBox(
+          width: 20,
+        ),
         Slider(
-          activeColor: Color.fromRGBO(109, 152, 134, 1),
+          activeColor: const Color.fromRGBO(109, 152, 134, 1),
           value: _currentSliderValue,
           max: 1,
           divisions: 20,
